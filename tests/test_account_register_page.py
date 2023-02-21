d63a5c86f020befab4c54f8c9fe30af24245e552
DariaMelnikova27 <54035863+DariaMelnikova27@users.noreply.github.com>
2023-02-21 22:36:47 +0300

    homework12

--- tests/test_account_register_page.py
+++ tests/test_account_register_page.py
@@ -5,10 +5,18 @@ from allure_commons.types import Severity
 """ Тесты страницы регистрации нового пользователя """
 
 
-@allure.severity(severity_level=Severity.CRITICAL)
+@allure.severity(severity_level=Severity.BLOCKER)
 @allure.feature('Регистрация нового пользователя')
-@allure.title('Проверка видимости элементов на странице регистрации нового пользователя')
-def test_account_register_page(browser):
+@allure.title('Регистрация нового пользователя в магазине опенкарта')
+def test_new_user_register(browser):
     page = AccountRegisterPage(browser)
     page.open_page(browser)
-    page.check_elements_on_account_register_page()
+    page.firstname_fill("Ivan") \
+        .lastname_fill("Petrov") \
+        .email_fill("123@ya.ru") \
+        .telephone_fill("12345678") \
+        .password_fill("Qwe12345") \
+        .password_confirm_fill("Qwe12345") \
+        .checkbox_agree_click() \
+        .continue_button_click()
+    assert "Your Account Has Been Created!" in page.success_register_message
